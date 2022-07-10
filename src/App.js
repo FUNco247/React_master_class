7f2adb4a6f9efccfa61b829e175dc2ba9e8c3867
FUNco <crispy.alc@gmail.com>
2022-07-10 23:53:34 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Parent = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Rotation = keyframes`
+0% {
+  transform: rotate(odeg);
+  border-radius: 0px
+}
+50%{
+  border-radius: 150px;
+}
+100% {
+  transform: rotate(360deg)
+}`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 300px;
+  height: 300px;
+  animation: ${Rotation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: white;
+    &:hover {
+      color: black;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parent>
+        <Box bgColor="tomato">
+          <h1>Loading...</h1>
+        </Box>
+      </Parent>
     </div>
   );
 }
