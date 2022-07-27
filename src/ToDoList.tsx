15227707581f13c9c2991b37273e63f2e545c6a8
FUNco <crispy.alc@gmail.com>
2022-07-28 01:02:52 +0900

    react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  const { register, watch, handleSubmit, formState } = useForm();
+  const logData = (data: any) => {
+    console.log(data);
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(formState.errors);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form onSubmit={handleSubmit(logData)}>
+        <input
+          {...register("sampleInput1", {
+            required: "이 필드를 반드시 입력하세요",
+          })}
+          placeholder="Write a to do"
+        />
+        <input
+          {...register("sampleInput2", { required: true })}
+          placeholder="Write a to do"
+        />
+        <input
+          {...register("sampleInput3", { required: true, minLength: 5 })}
+          placeholder="Write a to do"
+        />
+        <input
+          {...register("sampleInput4", {
+            required: "이 필드를 반드시 입력하세요",
+            minLength: { value: 5, message: "5자 이상 입력하세요" },
+          })}
+          placeholder="Write a to do"
+        />
+        <input
+          {...register("sampleInput5", { required: true })}
+          placeholder="Write a to do"
+        />
         <button>Add</button>
       </form>
     </div>
